86806021f601200ce88227780c08ded9475cbdbb
Nrlsb <luksbenitez01@hotmail.com>
2025-07-02 06:20:50 -0300

    Correccion de error V2

--- backend/server.js
+++ backend/server.js
@@ -16,15 +16,18 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // 3. MIDDLEWARE GLOBAL
+const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:3000'];
 
-// --- INICIO DE LA CORRECCIÓN DE CORS ---
-const corsOptions = {
-  origin: process.env.FRONTEND_URL, // Permite solo peticiones desde la URL del frontend
-  optionsSuccessStatus: 200 
-};
-app.use(cors(corsOptions));
-// --- FIN DE LA CORRECCIÓN DE CORS ---
-
+app.use(cors({
+  origin: function (origin, callback) {
+    // permitir apps sin origin (como Postman) o las de nuestra lista
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 app.use(express.json());
 
 // 4. CONEXIÓN A LA BASE DE DATOS
